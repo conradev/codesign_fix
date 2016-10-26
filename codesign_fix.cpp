9fc3848e3cb5a49de705c104031c3529fbfee20d
Conrad Kramer <conrad@kramerapps.com>
2016-10-26 16:55:38 -0700

    Whitespace

--- codesign_fix.cpp
+++ codesign_fix.cpp
@@ -8,12 +8,12 @@ enum {
 struct TPCertGroup;
 
 static CSSM_RETURN fake_check(TPCertGroup &certGroup) {
-	return CSSM_OK;
+    return CSSM_OK;
 }
 
 __attribute__((constructor))
 static void fix_all_the_things() {
-	MSImageRef image = MSGetImageByName("/System/Library/Frameworks/Security.framework/Security");
-	CSSM_RETURN (*tpCheckCertificateAllowList)(TPCertGroup &) = (CSSM_RETURN (*)(TPCertGroup &))MSFindSymbol(image, "_tpCheckCertificateAllowList");
+    MSImageRef image = MSGetImageByName("/System/Library/Frameworks/Security.framework/Security");
+    CSSM_RETURN (*tpCheckCertificateAllowList)(TPCertGroup &) = (CSSM_RETURN (*)(TPCertGroup &))MSFindSymbol(image, "_tpCheckCertificateAllowList");
     MSHookFunction(tpCheckCertificateAllowList, &fake_check);
 }
